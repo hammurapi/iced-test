a7ffb2a229a70d90c5f639bf5381bc8dbc2a0903
hammurapi <3584715+hammurapi@users.noreply.github.com>
2024-02-03 00:27:56 +0100

    "Hello world!!" text box

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::text, Sandbox, Settings};
+
+struct MyApplication;
+
+#[derive(Debug)]
+enum MyMessage {}
+
+impl Sandbox for MyApplication {
+    type Message = MyMessage;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        String::from("iced test")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {}
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        text("Hello world").into()
+    }
+}
+
+fn main() -> iced::Result {
+    // println!("Hello, world!");
+
+    MyApplication::run(Settings::default())
 }
